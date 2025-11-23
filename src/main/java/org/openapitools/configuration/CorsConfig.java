c4ac7d50ddf25bd65a594b9d5fdfa7e3726e4ef9
irodriguezr04 <ismaelrodriguezrico04@gmail.com>
2025-11-23 11:43:40 +0100

    Conexión con el FrontEnd

---
+++ src/main/java/org/openapitools/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package org.openapitools.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("http://localhost:5173") // Permite SOLO a tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Métodos permitidos
+                        .allowedHeaders("*") // Cabeceras permitidas
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
