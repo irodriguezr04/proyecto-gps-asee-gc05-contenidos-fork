760516ac14fa56317c653ad794428caa9243a450
IsmaelMM13 <imiguelm@alumnos.unex.es>
2025-11-24 19:06:35 +0100

    Solucionados errores con la BD
    Se han solucionado algunos errores de la base de datos con respecto a las tildes de algunos atributos.
    También se ha cambiado el nombre de algunos atributos para que sean más coincidentes con los del esquema de OpenAPI

--- deployments/init/contenido_init.sql
+++ deployments/init/contenido_init.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict DyxaBMN8x5dPJUlSpRPVkyHRnECUDP23uDp7EyjMxNgkc6IoHzpa0f6zUSEvIxY
+\restrict 59CxA3Th3spWDd3aT6cPSyhMrzQS06S1C0TkRCEsmokGl94VLRTqBdeNksxguMp
 
 -- Dumped from database version 16.11 (Debian 16.11-1.pgdg13+1)
 -- Dumped by pg_dump version 16.11 (Debian 16.11-1.pgdg13+1)
@@ -31,8 +31,8 @@ CREATE TABLE public.albums (
     artist_id integer NOT NULL,
     label_id integer,
     title character varying(255) NOT NULL,
-    publishedat date,
-    coverurl character varying(255)
+    release_date date,
+    cover_url character varying(255)
 );
 
 
@@ -67,7 +67,8 @@ CREATE TABLE public.artists (
     id bigint NOT NULL,
     name character varying(255) NOT NULL,
     description text,
-    genre character varying(100)
+    genre character varying(100),
+    user_id character varying(255)
 );
 
 
@@ -138,8 +139,8 @@ CREATE TABLE public.tracks (
     title character varying(255) NOT NULL,
     genre character varying(100) NOT NULL,
     duration interval,
-    fileurl character varying(255),
-    publishedat timestamp without time zone
+    file_url character varying(255),
+    published_at timestamp without time zone
 );
 
 
@@ -198,11 +199,8 @@ ALTER TABLE ONLY public.tracks ALTER COLUMN id SET DEFAULT nextval('public.track
 -- Data for Name: albums; Type: TABLE DATA; Schema: public; Owner: usuario
 --
 
-COPY public.albums (id, artist_id, label_id, title, publishedat, coverurl) FROM stdin;
-1	1	1	A Night at the Opera	1975-11-21	http://url-de-ejemplo.com/cover.jpg
-2	2	2	Back in Black	1980-07-25	http://url-ejemplo.com/acdc-cover.jpg
+COPY public.albums (id, artist_id, label_id, title, release_date, cover_url) FROM stdin;
 3	3	3	Kind of Blue	1959-08-17	https://m.media-amazon.com/images/I/71UUU3OLX2L._UF894,1000_QL80_.jpg
-4	4	4	Por la boca vive el pez	2006-09-11	https://m.media-amazon.com/images/I/91iGr1p7SBL._UF350,350_QL50_.jpg
 5	5	5	Metallica (The Black Album)	1991-08-12	https://mariskalrock.com/wp-content/uploads/2018/09/metallica-the-black-album-web.jpg
 7	7	7	Beethoven: The Essentials	1985-10-15	https://images.universal-music.de/img/assets/446/446845/4/1280/beethoven-the-essentials.jpg
 8	8	8	True	2013-09-13	https://m.media-amazon.com/images/I/61Nol3quayL.jpg
@@ -213,6 +211,9 @@ COPY public.albums (id, artist_id, label_id, title, publishedat, coverurl) FROM 
 14	14	14	The Eminem Show	2002-05-26	https://m.media-amazon.com/images/I/61jxguiMreL.jpg
 15	15	15	Back to Rockport	2020-08-28	https://m.media-amazon.com/images/I/61Qtf00b3IL._UF894,1000_QL80_.jpg
 16	16	16	Nothing but the Beat	2011-08-26	https://m.media-amazon.com/images/I/710ZkhTLgJL._UF894,1000_QL80_.jpg
+4	4	4	Por la boca vive el pez	2006-09-11	https://m.media-amazon.com/images/I/91iGr1p7SBL._UF350,350_QL50_.jpg
+1	1	1	A Night at the Opera	1975-11-21	https://m.media-amazon.com/images/I/71nxRqKGA8L.jpg
+2	2	2	Back in Black	1980-07-25	https://upload.wikimedia.org/wikipedia/commons/9/92/ACDC_Back_in_Black.png
 \.
 
 
@@ -220,21 +221,21 @@ COPY public.albums (id, artist_id, label_id, title, publishedat, coverurl) FROM 
 -- Data for Name: artists; Type: TABLE DATA; Schema: public; Owner: usuario
 --
 
-COPY public.artists (id, name, description, genre) FROM stdin;
-1	Queen	Legendary British rock band	Rock
-2	AC/DC	Legendary Australian hard rock band formed in Sydney	Hard Rock
-3	Miles Davis	American jazz trumpeter, bandleader, and composer	Jazz
-4	Fito & Fitipaldis	Spanish rock band formed by Fito Cabrales	Pop Rock
-5	Metallica	American heavy metal band formed in Los Angeles	Metal
-7	Ludwig van Beethoven	German composer and pianist, a crucial figure in the transition between the Classical and Romantic eras	Cl├ísica
-8	Avicii	Swedish DJ, remixer, and music producer known for melodic house	Electr├│nica
-9	Bad Bunny	Puerto Rican rapper and singer, known as the King of Latin Trap	Urbano
-10	Dire Straits	British rock band led by Mark Knopfler, known for their distinct sound and guitar style	Rock
-11	Arctic Monkeys	English rock band formed in Sheffield, famous for their internet-driven rise	Indie
-13	Aitana	Spanish singer and songwriter, gained national recognition in Operaci├│n Triunfo	Pop
-14	Eminem	American rapper, songwriter, and record producer, cited as one of the greatest of all time	Urbano
-15	Kidd Keo	Spanish trap artist known for his bilingual flow mixing English and Spanish	Urbano
-16	David Guetta	French DJ and music producer, considered the grandfather of EDM	Electr├│nica
+COPY public.artists (id, name, description, genre, user_id) FROM stdin;
+1	Queen	Legendary British rock band	Rock	\N
+2	AC/DC	Legendary Australian hard rock band formed in Sydney	Hard Rock	\N
+3	Miles Davis	American jazz trumpeter, bandleader, and composer	Jazz	\N
+4	Fito & Fitipaldis	Spanish rock band formed by Fito Cabrales	Pop Rock	\N
+5	Metallica	American heavy metal band formed in Los Angeles	Metal	\N
+7	Ludwig van Beethoven	German composer and pianist, a crucial figure in the transition between the Classical and Romantic eras	Cl├ö├Â┬úÔö£┬ísica	\N
+8	Avicii	Swedish DJ, remixer, and music producer known for melodic house	Electr├ö├Â┬ú├ö├Â├®nica	\N
+9	Bad Bunny	Puerto Rican rapper and singer, known as the King of Latin Trap	Urbano	\N
+10	Dire Straits	British rock band led by Mark Knopfler, known for their distinct sound and guitar style	Rock	\N
+11	Arctic Monkeys	English rock band formed in Sheffield, famous for their internet-driven rise	Indie	\N
+13	Aitana	Spanish singer and songwriter, gained national recognition in Operaci├ö├Â┬ú├ö├Â├®n Triunfo	Pop	\N
+14	Eminem	American rapper, songwriter, and record producer, cited as one of the greatest of all time	Urbano	\N
+15	Kidd Keo	Spanish trap artist known for his bilingual flow mixing English and Spanish	Urbano	\N
+16	David Guetta	French DJ and music producer, considered the grandfather of EDM	Electr├ö├Â┬ú├ö├Â├®nica	\N
 \.
 
 
@@ -264,7 +265,7 @@ COPY public.labels (id, name, description) FROM stdin;
 -- Data for Name: tracks; Type: TABLE DATA; Schema: public; Owner: usuario
 --
 
-COPY public.tracks (id, album_id, title, genre, duration, fileurl, publishedat) FROM stdin;
+COPY public.tracks (id, album_id, title, genre, duration, file_url, published_at) FROM stdin;
 1	1	Bohemian Rhapsody	Rock	00:05:55	http://files.com/track1.mp3	2025-11-19 18:03:25.207978
 2	1	You're My Best Friend	Pop Rock	00:02:52	http://files.com/track2.mp3	2025-11-19 18:03:25.207978
 3	1	Love of My Life	Ballad	00:03:39	http://files.com/track3.mp3	2025-11-19 18:03:25.207978
@@ -274,18 +275,11 @@ COPY public.tracks (id, album_id, title, genre, duration, fileurl, publishedat) 
 7	3	So What	Jazz	00:09:22	\N	2025-11-23 21:10:22.217977
 8	3	Freddie Freeloader	Jazz	00:09:46	\N	2025-11-23 21:10:22.217977
 9	4	Por la boca vive el pez	Rock	00:04:13	\N	2025-11-23 21:14:01.399583
-10	4	Me equivocar├¡a otra vez	Pop Rock	00:05:20	\N	2025-11-23 21:14:01.399583
 11	4	Abrazado a la tristeza	Rock	00:04:45	\N	2025-11-23 21:14:01.399583
 18	5	Enter Sandman	Metal	00:05:31	\N	2025-11-23 21:25:55.033773
 19	5	Nothing Else Matters	Ballad	00:06:28	\N	2025-11-23 21:25:55.033773
 20	5	Sad But True	Metal	00:05:24	\N	2025-11-23 21:25:55.033773
-21	7	Symphony No. 5: Allegro con brio	Cl├ísica	00:07:18	\N	2025-11-23 21:27:54.37997
-22	7	F├╝r Elise (Bagatelle No. 25)	Cl├ísica	00:03:25	\N	2025-11-23 21:27:54.37997
-23	7	Moonlight Sonata (Adagio sostenuto)	Cl├ísica	00:06:00	\N	2025-11-23 21:27:54.37997
-24	8	Wake Me Up	Electr├│nica	00:04:07	\N	2025-11-23 21:29:40.14986
-25	8	Hey Brother	Electr├│nica	00:04:15	\N	2025-11-23 21:29:40.14986
 26	8	Addicted to You	Pop	00:02:28	\N	2025-11-23 21:29:40.14986
-27	9	Tit├¡ Me Pregunt├│	Urbano	00:04:03	\N	2025-11-23 21:35:55.953306
 28	9	Me Porto Bonito	Urbano	00:02:58	\N	2025-11-23 21:35:55.953306
 29	9	Ojitos Lindos	Pop	00:04:30	\N	2025-11-23 21:35:55.953306
 30	10	Money for Nothing	Rock	00:08:26	\N	2025-11-23 21:38:40.131229
@@ -295,17 +289,24 @@ COPY public.tracks (id, album_id, title, genre, duration, fileurl, publishedat) 
 34	11	R U Mine?	Rock	00:03:21	\N	2025-11-23 21:41:37.52141
 35	11	I Wanna Be Yours	Ballad	00:03:04	\N	2025-11-23 21:41:37.52141
 36	13	11 Razones	Pop	00:03:33	\N	2025-11-23 21:44:30.350007
-37	13	+ (M├ís)	Pop	00:03:39	\N	2025-11-23 21:44:30.350007
-38	13	No te has ido y ya te extra├▒o	Pop	00:03:29	\N	2025-11-23 21:44:30.350007
 39	14	Without Me	Urbano	00:04:50	\N	2025-11-23 21:46:58.857144
 40	14	Sing for the Moment	Rock	00:05:40	\N	2025-11-23 21:46:58.857144
 41	14	Till I Collapse	Urbano	00:04:57	\N	2025-11-23 21:46:58.857144
 42	15	Dracukeo	Urbano	00:03:40	\N	2025-11-23 21:48:39.707357
 43	15	Moon Talk	Pop	00:03:20	\N	2025-11-23 21:48:39.707357
-44	15	Ma Vie	Electr├│nica	00:03:45	\N	2025-11-23 21:48:39.707357
-45	16	Titanium (feat. Sia)	Electr├│nica	00:04:05	\N	2025-11-23 21:52:24.713969
 46	16	Little Bad Girl (feat. Ludacris)	Urbano	00:03:12	\N	2025-11-23 21:52:24.713969
 47	16	Without You (feat. Usher)	Pop	00:03:28	\N	2025-11-23 21:52:24.713969
+21	7	Symphony No. 5: Allegro con brio	ClÔö£├¡sica	00:07:18	\N	2025-11-23 21:27:54.37997
+23	7	Moonlight Sonata (Adagio sostenuto)	ClÔö£├¡sica	00:06:00	\N	2025-11-23 21:27:54.37997
+24	8	Wake Me Up	ElectrÔö£Ôöénica	00:04:07	\N	2025-11-23 21:29:40.14986
+25	8	Hey Brother	ElectrÔö£Ôöénica	00:04:15	\N	2025-11-23 21:29:40.14986
+44	15	Ma Vie	ElectrÔö£Ôöénica	00:03:45	\N	2025-11-23 21:48:39.707357
+45	16	Titanium (feat. Sia)	ElectrÔö£Ôöénica	00:04:05	\N	2025-11-23 21:52:24.713969
+10	4	Me equivocarÔö£┬ía otra vez	Pop Rock	00:05:20	\N	2025-11-23 21:14:01.399583
+22	7	Fur Elise (Bagatelle No. 25)	ClÔö£├¡sica	00:03:25	\N	2025-11-23 21:27:54.37997
+27	9	TitÔö£┬í Me PreguntÔö£Ôöé	Urbano	00:04:03	\N	2025-11-23 21:35:55.953306
+37	13	+ (MÔö£├¡s)	Pop	00:03:39	\N	2025-11-23 21:44:30.350007
+38	13	No te has ido y ya te extraÔö£ÔûÆo	Pop	00:03:29	\N	2025-11-23 21:44:30.350007
 \.
 
 
@@ -397,5 +398,5 @@ ALTER TABLE ONLY public.tracks
 -- PostgreSQL database dump complete
 --
 
-\unrestrict DyxaBMN8x5dPJUlSpRPVkyHRnECUDP23uDp7EyjMxNgkc6IoHzpa0f6zUSEvIxY
+\unrestrict 59CxA3Th3spWDd3aT6cPSyhMrzQS06S1C0TkRCEsmokGl94VLRTqBdeNksxguMp
 
