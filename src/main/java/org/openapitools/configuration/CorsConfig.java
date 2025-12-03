eaa87d74dc84426c53cbd4712fdae8e102ceccd5
irodriguezr04 <ismaelrodriguezrico04@gmail.com>
2025-12-04 00:28:12 +0100

    refactor: Corregir incidencias detectadas por SonarCloud 2

--- src/main/java/org/openapitools/configuration/CorsConfig.java
+++ src/main/java/org/openapitools/configuration/CorsConfig.java
@@ -1,24 +1,24 @@
-package org.openapitools.configuration;
+// package org.openapitools.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("http://localhost:5173") // Permite SOLO a tu frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Métodos permitidos
-                        .allowedHeaders("*") // Cabeceras permitidas
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**") // Aplica a todas las rutas
+//                         .allowedOrigins("http://localhost:5173") // Permite SOLO a tu frontend
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Métodos permitidos
+//                         .allowedHeaders("*") // Cabeceras permitidas
+//                         .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
